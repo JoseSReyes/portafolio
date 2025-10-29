24d5a02b52dbda66a655e421831f436e88cee951
JoseSReyes <92191906+JoseSReyes@users.noreply.github.com>
2025-10-29 13:49:29 -0600

    add files
    Add my files

---
+++ script.js
@@ -0,0 +1,23 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+const year = document.getElementById("year");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+
+year.textContent = new Date().getFullYear();
